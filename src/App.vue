e769e976db7fc4c108c6a711e66fec912785f679
Ben Wu <wubin28@gmail.com>
2023-09-13 16:54:04 +0800

    feat: add background color to the todo list

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,14 @@ export default class App extends Vue {}
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+.el-container {
+  background-color: #e9eef3;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
