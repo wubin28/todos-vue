810de57d6f3d749db777c082244413ac418eb4fa
Ben Wu <wubin28@gmail.com>
2023-09-14 09:42:35 +0800

    feat: get todos from the back end

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -9,6 +9,20 @@ import {ElMessage} from "element-plus";
   }
 })
 export default class TodoList extends Vue {
+  todos = [];
+
+  async mounted() {
+    await this.loadTodos();
+  }
+
+  async loadTodos() {
+    try {
+      const response = await this.axios.get("http://192.168.31.180:8081/api/v1/todos");
+      this.todos = response.data;
+    } catch (error) {
+      console.log('An error occurred:', error);
+    }
+  }
   async createTodo(todo: any) {
     console.log("Todo", todo)
     ElMessage({
@@ -20,8 +34,12 @@ export default class TodoList extends Vue {
 </script>
 
 <template>
-  <h1>TodoList</h1>
-  <todo-form @send-message="createTodo"></todo-form>
+  <el-row>
+    <el-col :span="12" :offset="7" style="width: 100%">
+      <h1>TodoList</h1>
+      <todo-form @send-message="createTodo"></todo-form>
+    </el-col>
+  </el-row>
 </template>
 
 <style scoped>
